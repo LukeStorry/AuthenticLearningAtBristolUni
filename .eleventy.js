6841b3b93c0706f52c8e85012bda9e6210f4a318
Luke Storry <luke.storry@ghyston.com>
2020-07-19 22:19:36 +0100

    added prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,34 @@
 var nestedAccordion = require("nested-accordion");
 var authenticLearningDataFile = require("./data.json");
 
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addShortcode("authentic_learning_accordion", () => {
     var data = JSON.parse(authenticLearningDataFile.data);
-    data.forEach((location) => delete location.text); // remove gps coordinates
+    data.forEach(location => delete location.text); // remove gps coordinates
     return nestedAccordion.create(data);
   });
 
-  const createGeoFeature = (location) => ({
+  const createGeoFeature = location => ({
     type: "Feature",
     properties: {
       name: location.title,
       html: `<h6>${location.title}</h6>
       ${nestedAccordion.create(location.children)}`,
-      size: [].concat(...location.children).length + 2,
+      size: [].concat(...location.children).length + 2
     },
     geometry: {
       type: "Point",
-      coordinates: location.text.split(",").map(parseFloat).sort(),
-    },
+      coordinates: location.text
+        .split(",")
+        .map(parseFloat)
+        .sort()
+    }
   });
 
   eleventyConfig.addShortcode("authentic_learning_geojson", () => {
     const geojson = {
       type: "FeatureCollection",
-      features: JSON.parse(authenticLearningDataFile.data).map(
-        createGeoFeature
-      ),
+      features: JSON.parse(authenticLearningDataFile.data).map(createGeoFeature)
     };
 
     return JSON.stringify(geojson);
