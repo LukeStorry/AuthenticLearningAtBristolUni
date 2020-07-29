a39cd6e8c02144a92c84e92e3837789f9d8bb7d7
Luke Storry <luke.storry@ghyston.com>
2020-07-29 11:07:14 +0100

    changed to sidebar not popup

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,43 @@
 var nestedAccordion = require("nested-accordion");
 var authenticLearningDataFile = require("./data.json");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("authentic_learning_accordion", () => {
     var data = JSON.parse(authenticLearningDataFile.data);
-    data.forEach(location => delete location.text); // remove gps coordinates
+    data.forEach((location) => delete location.text); // remove gps coordinates
     return nestedAccordion.create(data);
   });
 
-  const createGeoFeature = location => ({
-    type: "Feature",
-    properties: {
-      name: location.title,
-      html: `<h6>${location.title}</h6>
-      ${nestedAccordion.create(location.children)}`,
-      size: [].concat(...location.children).length + 2
-    },
-    geometry: {
-      type: "Point",
-      coordinates: location.text
-        .split(",")
-        .map(parseFloat)
-        .sort()
-    }
-  });
+  const createGeoFeature = (location) => {
+    const nestedAccordionHtml = nestedAccordion.create(location.children);
+    return {
+      type: "Feature",
+      properties: {
+        name: location.title,
+        html: `<h2>${location.title}</h2>
+      ${nestedAccordionHtml}`,
+        size: nestedAccordionHtml.length,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: location.text.split(",").map(parseFloat).sort(),
+      },
+    };
+  };
 
   eleventyConfig.addShortcode("authentic_learning_geojson", () => {
     const geojson = {
       type: "FeatureCollection",
-      features: JSON.parse(authenticLearningDataFile.data).map(createGeoFeature)
+      features: JSON.parse(authenticLearningDataFile.data).map(
+        createGeoFeature
+      ),
     };
 
     return JSON.stringify(geojson);
   });
 
   eleventyConfig.addShortcode("accordion_styles", () =>
-    nestedAccordion.styles()
+  nestedAccordion.styles()
   );
 
   eleventyConfig.addPassthroughCopy("admin");
